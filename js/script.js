7f3e66e3dcbee8abe0da013e3369b1522e38e7c7
roman <roman026@gmail.com>
2022-09-20 01:08:45 -0400

    si hiso algunas mejoras en las funcionalidades

--- js/script.js
+++ js/script.js
@@ -10,28 +10,31 @@ const $btnInicio = document.getElementById("btnIniciar"),
   $acierto = document.getElementById("acierto"),
   $msgFinal = document.getElementById("msg-final"),
   $juego = document.getElementById("juego");
+const ctx = document.getElementById("canvas").getContext("2d");
 
 var palSelect = "";
-
 let Victoria = 0;
 let count = 0;
-/* var palabra; */
-var rand;
 
 const categoria = () => {
-  /*   var palabra; */
   var palabra = $categoria.value;
   opccion = eval(palabra);
   rand = [Math.floor(Math.random() * opccion.length)];
   palSelect = opccion[rand][0].toUpperCase();
   ayuda = opccion[rand][1];
 };
+const desbloquear = () => {
+  let letterButtons = document.querySelectorAll(".letra");
+
+  letterButtons.forEach((button) => {
+    button.disabled = false;
+  });
+};
 const blocker = () => {
   let letterButtons = document.querySelectorAll(".letra");
 
   letterButtons.forEach((button) => {
     button.disabled = true;
-    console.log(button);
   });
 };
 function pista() {
@@ -42,120 +45,92 @@ function oculta() {
   $palabraOculta.innerHTML = displayItem;
 }
 
-/* function botonesLetras(a, z) {
-  document.getElementById("abcdario").innerHTML = "";
-  let i = a.charCodeAt(0),
-    j = z.charCodeAt(0);
-  let letra = "";
-  for (; i <= j; i++) {
-    letra = String.fromCharCode(i).toUpperCase();
-    document.getElementById("abcdario").innerHTML +=
-      "<button value='" +
-      letra +
-      "' onclick='intento(\"" +
-      letra +
-      "\")' class='letra' id='" +
-      letra +
-      "'>" +
-      letra +
-      "</button>";
+function generarAbcdario() {
+  for (let i = 97; i < 123; i++) {
+    let button = document.createElement("button");
+    //se genera el abecedario
+    button.innerText = String.fromCharCode(i).toUpperCase();
+    button.classList.add("letra");
+    abcdario.append(button);
 
     if (i == 110) {
-      document.getElementById("abcdario").innerHTML +=
-        "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='Ñ'>Ñ</button>";
+      button = document.createElement("button");
+      button.classList.add("letra");
+      button.innerText = "Ñ";
+      abcdario.append(button);
     }
     if (i == 122) {
-      document.getElementById("abcdario").innerHTML +=
-        "<button value=' ' onclick='intento(\" \")' class='letra' id=' '>__</button>";
+      button = document.createElement("button");
+      button.classList.add("letra");
+      button.innerText = "-";
+      abcdario.append(button);
     }
   }
 }
- */
+
 const empezar = () => {
   Victoria = 0;
   count = 0;
   $palabraOculta.innerHTML = "";
-  /*  letterContainer.classList.add("hide");
-  newGameContainer.classList.add("hide");
-  letterContainer.innerHTML = ""; */
-  //For creating letter buttons
-  for (let i = 97; i < 125; i++) {
-    let button = document.createElement("button");
-    //Number to ASCII[A-Z]
-    button.innerText = String.fromCharCode(i).toUpperCase();
-    button.classList.add("letra");
-    if (i == 123) {
-      button = document.createElement("button");
-      button.classList.add("letra");
-      button.innerText = "Ñ";
-      console.log(button);
-    }
-    if (i == 124) {
-      button = document.createElement("button");
-      button.classList.add("letra");
-      button.innerText = " ";
-      console.log(button);
-    }
+  let letra = document.querySelectorAll(".letra");
 
-    //character button click
+  letra.forEach((button) => {
     button.addEventListener("click", () => {
       let charArray = palSelect.split("");
       let guion = document.getElementsByClassName("oculta");
 
-      //if array contains clciked value replace the matched dash with letter else dram on canvas
+      //si el contenido del array es clikeado correctamente o sino se pinta el hombre en el canvas
       if (charArray.includes(button.innerText)) {
         charArray.forEach((char, index) => {
-          //if character in array is same as clicked button
+          //si verifica si el caracter del array es el clikeado
           if (char === button.innerText) {
-            //replace dash with letter
+            //se remplaza lo guines por la letra
             guion[index].innerText = char;
             $acierto.innerHTML = "¡Muy Bien!";
             $acierto.className += "acierto verde";
-            //increment counter
+            //se incrementa el contador de vistoria
 
             Victoria += 1;
-            //if winCount equals word lenfth
+            //si la longitud de es igual a victoria gano
             if (Victoria == charArray.length) {
               $msgFinal.innerHTML = `😀 😎 GANASTE !!<br> La palabra era<br> <hr>${palSelect}`;
-              $msgFinal.className += "zoom-in verde";
-              //block all buttons
+              $msgFinal.className = "zoom-in verde";
+              //se llama a la funcion para bloquear todos los botones
               blocker();
             }
           }
         });
       } else {
-        //Mensaje de falla
+        //Mensaje de fallaste
         $mal.innerHTML = "¡Fallaste!";
         $mal.className += "acierto rojo";
 
         count += 1;
-        //for drawing man
-        drawMan(count);
-        //Count==6 because head,body,left arm, right arm,left leg,right leg
+        //se llama a la funcion dibujar hombre
+        dibujarHombre(count);
+        //si el contador es igual a 7 se da el mensaje de que ha perdido
         if (count == 7) {
           $msgFinal.innerHTML = `🙁 PERDISTE !!<br> La palabra era<br> <hr>${palSelect}`;
-          $msgFinal.className += "zoom-in rojo";
+          $msgFinal.className = "zoom-in rojo";
           blocker();
         }
       }
-      //disable clicked button
+      //se deshabilita el boton clikeado
       button.disabled = true;
       setTimeout(function () {
         $acierto.className = "";
         $mal.className = "";
       }, 800);
     });
-
-    abcdario.append(button);
-  }
-
+  });
   categoria();
-  //Call to canvasCreator (for clearing previous canvas and creating initial canvas)
+  //se llama al cancas
   let { ahorca } = canvas();
-  //initialDrawing would draw the frame
+  // se inicia con la escena de la ahorca
   ahorca();
 };
-const drawMan = (count) => {
+
+const dibujarHombre = (count) => {
   let { soga, cabeza, cuerpo, brazo_izq, brazo_der, pierna_izq, pierna_der } =
     canvas();
   switch (count) {
@@ -185,21 +160,30 @@ const drawMan = (count) => {
   }
 };
 
-$btnAyuda.onclick = async () => {
-  pista();
-};
 $btnInicio.onclick = async () => {
   $inicio.style.display = "none";
   $juego.style.display = "block";
+  generarAbcdario();
   empezar();
   oculta();
+};
+$btnNuevo.onclick = () => {
+  document.getElementById("ayuda").innerHTML = "";
+  ctx.clearRect(0, 0, 560, 600);
+  Victoria = 0;
+  count = 0;
+  let { ahorca } = canvas();
+  ahorca();
+  categoria();
+  oculta();
+  desbloquear();
+  $msgFinal.className = "hide";
 };
 $btnDesistir.onclick = async () => {
   location.reload();
   $inicio.style.display = "block";
   $juego.style.display = "none";
 };
-
-$btnNuevo.onclick = () => {
-  location.reload($inicio);
+$btnAyuda.onclick = async () => {
+  pista();
 };
